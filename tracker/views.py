881febbc7216c719c8f2b97b0b8723ec71765cfe
Mrinal <kmrinal19@gmail.com>
2020-05-21 14:40:49 +0530

    added exception handling for authentication

--- tracker/views.py
+++ tracker/views.py
@@ -8,12 +8,13 @@ from .serializers import *
 from tracker.permissions import *
 from rest_framework.permissions import IsAuthenticated
 import requests
-from django.http import HttpRequest
+from django.urls import reverse
 
 from django.contrib.auth import login
 from rest_framework import permissions
 from knox.views import LoginView as KnoxLoginView
 
+from rest_framework.exceptions import ParseError
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
@@ -58,9 +59,19 @@ class AuthView(APIView):
         auth_object = Auth(code)
         serializer = AuthSerializer(auth_object)
         token_response = requests.post('https://internet.channeli.in/open_auth/token/', data=serializer.data)
+
         if token_response.status_code == 200:
-            login_response = requests.post('http://127.0.0.1:8000/tracker/api-auth/login/', data = token_response.json())
+            login_response = requests.post('http://localhost:8000'+reverse('knox_login'), data = token_response.json())
+
+            if login_response.status_code == 200:
+                login_response = login_response.json()
+                user = User.objects.get(userId =login_response['user']['userId'])
+                user_serializer = UserSerializer(user)
+                user_data = {'token':login_response['token'], 'expiry':login_response['expiry'], 'user':user_serializer.data}
+                return Response (user_data)
+
             return Response(login_response.json())
+
         return Response(token_response.json())
 
 class LoginView(KnoxLoginView):
@@ -68,42 +79,47 @@ class LoginView(KnoxLoginView):
 
     def post(self, request, format=None):
 
-        AUTH_TOKEN = 'Bearer '+request.data['access_token']
-        headers = {'Authorization': AUTH_TOKEN}
-        url = 'https://internet.channeli.in/open_auth/get_user_data/'
-        data_response = requests.get(url, headers=headers)
+        try:
+            AUTH_TOKEN = 'Bearer '+request.data['access_token']
+            headers = {'Authorization': AUTH_TOKEN}
+            url = 'https://internet.channeli.in/open_auth/get_user_data/'
+            data_response = requests.get(url, headers=headers)
 
-        if data_response.status_code == 200:
-            data_response = data_response.json()
-            userId =  data_response['userId']
-            name = (data_response['person']['fullName']).strip()
-            email = data_response['contactInformation']['emailAddress']
-            phoneNumber = data_response['contactInformation']['primaryPhoneNumber']
-            try:
-                username = name[:name.index(' ')]+'_'+str(userId)
-            except ValueError:
-                username = name+'_'+str(userId)
-            try:
-                requestUser = User.objects.get(userId = userId)
-                serializer = AuthTokenSerializer(data={'userId': requestUser.userId})
-                serializer.is_valid(raise_exception=True)
-                user = serializer.validated_data['user']
-                login(request, user)
-                return super(LoginView, self).post(request, format=None)
-                
+            if data_response.status_code == 200:
+                data_response = data_response.json()
+                userId =  data_response['userId']
+                name = (data_response['person']['fullName']).strip()
+                email = data_response['contactInformation']['emailAddress']
+                phoneNumber = data_response['contactInformation']['primaryPhoneNumber']
+                enrollmentNumber = data_response['student']['enrolmentNumber']
+                try:
+                    username = name[:name.index(' ')]+'_'+str(userId)
+                except ValueError:
+                    username = name+'_'+str(userId)
+                try:
+                    requestUser = User.objects.get(userId = userId)
+                    serializer = AuthTokenSerializer(data={'userId': requestUser.userId})
+                    serializer.is_valid(raise_exception=True)
+                    user = serializer.validated_data['user']
+                    login(request, user)
+                    return super(LoginView, self).post(request, format=None)
+                    
 
-            except User.DoesNotExist:
-                requestUser = User(userId = userId,
-                    name = name,
-                    email = email,
-                    username = username,
-                    phoneNumber = phoneNumber,
-                    )
-                requestUser.save()
-                serializer = AuthTokenSerializer(data={'userId': requestUser.userId})
-                serializer.is_valid(raise_exception=True)
-                user = serializer.validated_data['user']
-                login(request, user)
-                return super(LoginView, self).post(request, format=None)
-        else:
-            return Response(data_response.json())
+                except User.DoesNotExist:
+                    requestUser = User(userId = userId,
+                        name = name,
+                        email = email,
+                        username = username,
+                        phoneNumber = phoneNumber,
+                        enrollmentNumber = enrollmentNumber,
+                        )
+                    requestUser.save()
+                    serializer = AuthTokenSerializer(data={'userId': requestUser.userId})
+                    serializer.is_valid(raise_exception=True)
+                    user = serializer.validated_data['user']
+                    login(request, user)
+                    return super(LoginView, self).post(request, format=None)
+            else:
+                return Response(data_response)
+        except KeyError:
+            raise ParseError
