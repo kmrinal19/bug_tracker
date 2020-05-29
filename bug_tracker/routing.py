86835916f20461705ae0bb418cccb55d8c7a4e6e
Mrinal <kmrinal19@gmail.com>
2020-05-30 00:41:59 +0530

    minor bug fix

--- bug_tracker/routing.py
+++ bug_tracker/routing.py
@@ -1,7 +1,6 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import tracker.routing
-from tracker.knoxAuthSockets import TokenAuthMiddleware
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
